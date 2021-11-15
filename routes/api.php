74a2d06060ecd0a2ee68435e3ddeb43e0338c8fb
sallieeky <sallieeky@gmail.com>
2021-11-15 16:06:55 +0800

    TES

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/", function () {
+    return response()->json([
+        "message" => "Welcome to the API"
+    ]);
+});
